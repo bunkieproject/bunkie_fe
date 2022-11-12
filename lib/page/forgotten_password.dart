16ef36c504e4392f6b3bbb7f5b153e4ba7c31aae
ituitis-canb18 <canbatuhan.official@gmail.com>
2022-11-12 19:21:36 +0300

    BUN-68 : code format

--- lib/page/forgotten_password.dart
+++ lib/page/forgotten_password.dart
@@ -28,80 +28,82 @@ class _ForgottenPasswordPageState extends State<ForgottenPasswordPage> {
                 color: BunkieColors.bright,
                 padding: const EdgeInsets.all(25),
                 child: Form(
-                  key: __forgottenPasswordFormKey,
-                  child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text("Reset your\npassword",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(color: BunkieColors.light),
-                        textScaleFactor: BunkieText.large),
-                    SizedBox(
-                      height: screenHeight * 0.05,
-                    ),
-                    TextFormField(
-                      validator: (value) {
+                    key: __forgottenPasswordFormKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text("Reset your\npassword",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(color: BunkieColors.light),
+                            textScaleFactor: BunkieText.large),
+                        SizedBox(
+                          height: screenHeight * 0.05,
+                        ),
+                        TextFormField(
+                            validator: (value) {
                               if (value!.isEmpty) {
                                 return "Please enter your email.";
                               } else {
                                 return null;
                               }
                             },
-                        decoration: const InputDecoration(
-                            filled: true,
-                            fillColor: BunkieColors.light,
-                            prefixIcon: Icon(Icons.email),
-                            labelText: "Email",
-                            border: UnderlineInputBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(12.5))))),
-                    SizedBox(
-                      height: screenHeight * 0.0125,
-                    ),
-                    TextFormField(
-                      validator: (value) {
+                            decoration: const InputDecoration(
+                                filled: true,
+                                fillColor: BunkieColors.light,
+                                prefixIcon: Icon(Icons.email),
+                                labelText: "Email",
+                                border: UnderlineInputBorder(
+                                    borderRadius: BorderRadius.all(
+                                        Radius.circular(12.5))))),
+                        SizedBox(
+                          height: screenHeight * 0.0125,
+                        ),
+                        TextFormField(
+                            validator: (value) {
                               if (value!.isEmpty) {
                                 return "Please enter your username.";
                               } else {
                                 return null;
                               }
                             },
-                        decoration: const InputDecoration(
-                            filled: true,
-                            fillColor: BunkieColors.light,
-                            prefixIcon: Icon(Icons.person),
-                            labelText: "Username",
-                            border: UnderlineInputBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(12.5))))),
-                    SizedBox(
-                      height: screenHeight * 0.05,
-                    ),
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(25.0),
-                      child: ElevatedButton(
-                          onPressed: () {
-                                if (__forgottenPasswordFormKey.currentState!.validate()) {
+                            decoration: const InputDecoration(
+                                filled: true,
+                                fillColor: BunkieColors.light,
+                                prefixIcon: Icon(Icons.person),
+                                labelText: "Username",
+                                border: UnderlineInputBorder(
+                                    borderRadius: BorderRadius.all(
+                                        Radius.circular(12.5))))),
+                        SizedBox(
+                          height: screenHeight * 0.05,
+                        ),
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(25.0),
+                          child: ElevatedButton(
+                              onPressed: () {
+                                if (__forgottenPasswordFormKey.currentState!
+                                    .validate()) {
                                   // TO DO : if form is valid, talk to backend
-                                  print("Talking to backend about Forgotten Password");
+                                  print(
+                                      "Talking to backend about Forgotten Password");
                                 }
-                              }, // TO DO : Tries to Confirm action, otherwise prints error
-                          style: ButtonStyle(
-                              padding: MaterialStateProperty.all(
-                                  const EdgeInsets.only(
-                                      top: 20,
-                                      right: 40,
-                                      bottom: 20,
-                                      left: 40)),
-                              backgroundColor:
-                                  MaterialStateProperty.all(BunkieColors.dark)),
-                          child: const Text("Reset",
-                              textAlign: TextAlign.center,
-                              style: TextStyle(color: BunkieColors.light),
-                              textScaleFactor: BunkieText.medium)),
-                    )
-                  ],
-                ))),
+                              },
+                              style: ButtonStyle(
+                                  padding: MaterialStateProperty.all(
+                                      const EdgeInsets.only(
+                                          top: 20,
+                                          right: 40,
+                                          bottom: 20,
+                                          left: 40)),
+                                  backgroundColor: MaterialStateProperty.all(
+                                      BunkieColors.dark)),
+                              child: const Text("Reset",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(color: BunkieColors.light),
+                                  textScaleFactor: BunkieText.medium)),
+                        )
+                      ],
+                    ))),
           ),
         ],
       )),
