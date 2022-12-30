4776a4d03c603720956d0959049001f34b03ddfb
ituitis-canb18 <canbatuhan.official@gmail.com>
2022-12-30 16:03:24 +0300

    BUN-92 : room/bunkie deafult search

--- lib/api/search.dart
+++ lib/api/search.dart
@@ -1,67 +1,89 @@
 import 'dart:convert' as json;
-import 'package:app/api/util.dart';
 import 'package:app/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class BunkieSearchAPI {
-  static Future<Column> getHouseAds(BuildContext context,
-      GlobalKey<FormState> formKey, Map<String, String> formData) async {
-    if (formKey.currentState!.validate()) {
-      try {
-        var response = await http.post(
-          Uri.parse(BunkieAddress.getRoute("ads/get_room_ad")),
+  static Future<dynamic> getHouseAds(
+      BuildContext context, Map<String, dynamic> formData) async {
+    try {
+      http.Response response;
+      if (!formData.containsKey("lower_price")) {
+        response = await http.get(
+          Uri.https(
+              BunkieAddress.host,
+              "/ads/search_room_ad_default",
+              formData.map(
+                (key, value) => MapEntry(key, value.toString()),
+              )),
           headers: <String, String>{
             'Content-Type': 'application/json; charset=UTF-8',
           },
-          body: json.jsonEncode(formData),
         );
-
-        if (response.statusCode == 200) {
-          Map<String, dynamic> responseMap = json.jsonDecode(response.body);
-          // TODO : crate column
-          return Column();
-        }
-
-        // TODO : crate column
-        return Column();
+      } else {
+        response = await http.get(
+          Uri.https(
+              BunkieAddress.host,
+              "/ads/search_room_ad_preferred",
+              formData.map(
+                (key, value) => MapEntry(key, value.toString()),
+              )),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+          },
+        );
       }
-      
-      catch (e) {
-        print(e.toString());
+
+      if (response.statusCode == 200) {
+        Iterable responseMap = json.jsonDecode(response.body);
+        return responseMap;
       }
+    } catch (e) {
+      print(e.toString());
     }
 
-    return Column();
+    return Iterable.generate(0);
   }
 
-  static Future<Column> getBunkieAds(BuildContext context,
-      GlobalKey<FormState> formKey, Map<String, String> formData) async {
-    if (formKey.currentState!.validate()) {
-      try {
-        var response = await http.post(
-          Uri.parse(BunkieAddress.getRoute("ads/get_bunkie")),
+  static Future<dynamic> getBunkieAds(
+      BuildContext context, Map<String, dynamic> formData) async {
+    try {
+      http.Response response;
+      if (!formData.containsKey("lower_price")) {
+       response = await http.get(
+          Uri.https(
+              BunkieAddress.host,
+              "/ads/search_bunkie_default",
+              formData.map(
+                (key, value) => MapEntry(key, value.toString()),
+              )),
           headers: <String, String>{
             'Content-Type': 'application/json; charset=UTF-8',
           },
-          body: json.jsonEncode(formData),
         );
-
-        if (response.statusCode == 200) {
-          Map<String, dynamic> responseMap = json.jsonDecode(response.body);
-          // TODO : crate column
-          return Column();
-        }
-
-        // TODO : crate column
-        return Column();
+      } else {
+        response = await http.get(
+          Uri.https(
+              BunkieAddress.host,
+              "/ads/search_bunkie_preferred",
+              formData.map(
+                (key, value) => MapEntry(key, value.toString()),
+              )),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+          },
+        );
       }
-      
-      catch (e) {
-        print(e.toString());
+
+      if (response.statusCode == 200) {
+        Iterable responseMap = json.jsonDecode(response.body);
+        return responseMap;
       }
+
+    } catch (e) {
+      print(e.toString());
     }
 
-    return Column();
+    return Iterable.generate(0);
   }
 }
